acf5e0821aa8013d8bbe429db0d8fb423f8e7fa2
Janus Þór Kristjánsson <Janus@Januss-MacBook-Pro.local>
2016-11-07 20:19:16 +0000

    Changes?

--- Week05.Core/Services/Services/CoursesServiceProvider.cs
+++ Week05.Core/Services/Services/CoursesServiceProvider.cs
@@ -34,8 +34,54 @@ namespace CoursesAPI.Services.Services
 		/// <returns>Should return basic information about the person.</returns>
 		public PersonDTO AddTeacherToCourse(int courseInstanceID, AddTeacherViewModel model)
 		{
-			// TODO: implement this logic!
-			return null;
+				// Error Checking if the courseID  is correctly
+				var course = (from c in _courseInstances.All()
+				where c.ID == courseInstanceID
+				select c
+				).FirstOrDefault();
+				if(course == null){
+					throw new AppObjectNotFoundException();
+				}
+
+
+				// Error checking if teacher really exists
+				var teacherExists = (from p in _persons.All()
+				where p.SSN == model.SSN
+				select p).FirstOrDefault();
+				if(teacherExists == null){
+					throw new AppObjectNotFoundException();
+				}
+
+				// Error checking fi that teacher is in the class or if the class has a teacher
+				var total = (from tr in _teacherRegistrations.All()
+				where tr.CourseInstanceID == courseInstanceID
+				select tr).FirstOrDefault();
+				if (total == null)
+				{
+					TeacherRegistration newRegistration = new TeacherRegistration{
+						SSN = model.SSN,
+						CourseInstanceID = courseInstanceID,
+						Type = model.Type
+					};
+
+					_teacherRegistrations.Add(newRegistration);
+					_uow.Save();
+					return (	from p in _persons.All()
+								where model.SSN == p.SSN
+								select new PersonDTO{
+									SSN = p.SSN,
+									Name = p.Name
+								}).FirstOrDefault();
+				}
+				else if(total.SSN == model.SSN)
+				{
+					throw new AppValidationException("PERSON_ALREADY_REGISTERED_TEACHER_IN_COURSE");
+				} else {
+					throw new AppValidationException("COURSE_ALREADY_HAS_A_MAIN_TEACHER");
+				}
+
+				throw new AppValidationException("PERSON_ALREADY_REGISTERED_TEACHER_IN_COURSE");
+
 		}
 
 		/// <summary>
